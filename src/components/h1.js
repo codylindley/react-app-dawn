871f06caaacdf9fe68ecf68fd4175bf8a8fb12e4
Cody Lindley <contact@codylindley.com>
2017-01-12 14:10:50 -0700

    updates

--- src/components/h1.js
+++ src/components/h1.js
@@ -1,15 +1,19 @@
 import React from 'react';
+
 // example of image
 import src from '../static/password.jpg';
-
+// example of CSS pulled in, that make use of an img
 import styles from './h1.css';
-// import { redFont } from "./h1.css";
 
 export default () => {
     return (
         <div>
-            <img src={src} alt="" />
-            <h1 className={styles.redFont}> Hello Worldd </h1>
+            <h1>look at h1.js component:</h1>
+
+            <p>How an image is referenced in a component:</p>
+            <p><img src={src} alt="" /></p>
+            <p>Use of CSS and and image defined in a CSS file:</p>
+            <h3 className={styles.redFont}>Hello World</h3>
         </div>
     );
 };
