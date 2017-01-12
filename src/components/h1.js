4d7f67b05d75f742fda25e6b6342dd8cf3aa150a
Cody Lindley <contact@codylindley.com>
2017-01-12 14:23:28 -0700

    updates

--- src/components/h1.js
+++ src/components/h1.js
@@ -3,7 +3,7 @@ import React from 'react';
 // example of image
 import src from '../static/password.jpg';
 // example of CSS pulled in, that make use of an img
-import styles from './h1.css';
+import styles from './h3.css';
 
 export default () => {
     return (
@@ -12,7 +12,7 @@ export default () => {
 
             <p>How an image is referenced in a component:</p>
             <p><img src={src} alt="" /></p>
-            <p>Use of CSS and and image defined in a CSS file:</p>
+            <p>The style below comes from a imported CSS file (imported file contains image reference, used in BG)</p>
             <h3 className={styles.redFont}>Hello World</h3>
         </div>
     );
